a3867d875be198dd23f6df8c4bb5cc6f96c35653
jeremy <vivelerockpoint0@hotmail.fr>
2022-01-04 15:58:42 +0100

    Datepicker text red fix & mat-icon-suffix theme

--- apps/market/src/app/search/painting/view/view.component.ts
+++ apps/market/src/app/search/painting/view/view.component.ts
@@ -1,5 +1,6 @@
 import { Component, ChangeDetectionStrategy, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { PaintingService } from '@locart/painting';
 import { Painting } from '@locart/model';
 import { DurationForm, filterDates, RentService } from '@locart/rent';
@@ -7,6 +8,7 @@ import { AuthService } from '@locart/auth';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { orderBy, startAt, where } from 'firebase/firestore';
 import { map } from 'rxjs/operators';
+import { cpuUsage } from 'process';
 
 
 
@@ -20,7 +22,7 @@ export class PaintingViewComponent {
   @ViewChild('success') success!: TemplateRef<unknown>;
   id = this.routes.snapshot.paramMap.get('paintingId');
   painting$ = this.service.valueChanges(this.id);
-  
+
   form = new DurationForm();
 
   filters$ = this.rentService.valueChanges([
@@ -37,18 +39,23 @@ export class PaintingViewComponent {
     private rentService: RentService,
     private routes: ActivatedRoute,
     private snackbar: MatSnackBar,
-  ) {}
+    private location: Location
+  ) { }
 
   async rent(painting: Painting) {
     const email = this.auth.user?.email;
+    const range = this.form.value
+    if (this.form.valid) this.form.reset() // stocking and reseting form value prior to calling the service prevents the datepick to display text in red
     if (!email) return alert('Vous devez être connecté');
     await this.rentService.add({
       email,
-      duration: this.form.value,
+      duration: range,
       type: 'painting',
       workId: painting.id,
     });
+    this.form.reset();
     this.snackbar.openFromTemplate(this.success, { duration: 3000 });
+    this.location.back()
   }
 
 }
